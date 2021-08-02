2239959d6bc3b63a7a6a7047fcff24d146b6ac91
demonjoub <demonjoub@gmail.com>
2021-08-02 22:28:12 +0700

    fix: gitignore

--- chat-service/src/read/read.service.ts
+++ chat-service/src/read/read.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, getManager } from 'typeorm';
+import { Repository, getManager, getMetadataArgsStorage } from 'typeorm';
 
 import { ReadRequest } from './../request/read.request';
 import { ReadEntity } from './../entity/read.entity'
+import { MessagesEntity } from 'src/entity/messages.entity';
+import { ConversationEntity } from 'src/entity/conversation.entity';
 
 @Injectable()
 export class ReadService {
   constructor(
     @InjectRepository(ReadEntity)
     private readonly readRepository: Repository<ReadEntity>,
+    @InjectRepository(MessagesEntity)
+    private readonly messageRepository: Repository<MessagesEntity>
   ) { }
-
 
   async read(body: ReadRequest): Promise<ReadEntity> {
     try {
@@ -19,7 +22,33 @@ export class ReadService {
       r.conversationId = body.conversationId;
       r.isread = body.isread
       r.readBy = body.member;
-    
+
+      const conversation = await getManager()
+        .createQueryBuilder(ConversationEntity, "conversation")
+        .where("conversation.conversationId = :conversationId", { conversationId: body.conversationId })
+        .getOne()
+
+      const messages = await getManager()
+        .createQueryBuilder(MessagesEntity, "msg")
+        .where("msg.conversationId = :conversationId", { conversationId: body.conversationId })
+        .orderBy("createAt", "DESC")
+        .getOne()
+
+      const read = await getManager()
+        .createQueryBuilder(ReadEntity, "read")
+        .where("read.conversationId = :conversationId", { conversationId: body.conversationId })
+        .getOne()
+      
+      const readByMessage = read?.readByMessage ? JSON.parse(read.readByMessage) : {}
+
+      const object = {
+        ...readByMessage,
+        [body.member]: {
+          messageId: messages.id,
+          isread: true
+        }
+      }
+      r.readByMessage = JSON.stringify(object)
       const res = await this.readRepository.save(r)
       return res;
     } catch (e) {
